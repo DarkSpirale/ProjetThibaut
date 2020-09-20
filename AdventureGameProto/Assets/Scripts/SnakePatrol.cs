28f2b072e42e96a794977408f2183eaa6835ab43
foxwit0 <foxwit.0@gmail.com>
2020-09-20 23:34:21 +0200

    SnakePatrol BoxCast

--- AdventureGameProto/Assets/Scripts/SnakePatrol.cs
+++ AdventureGameProto/Assets/Scripts/SnakePatrol.cs
@@ -5,6 +5,8 @@ public class SnakePatrol : MonoBehaviour
 {
     public Animator animator;
 
+    public BoxCollider2D myCollider;
+
     public int damageOnCollision;
 
     public float movementRadius = 3f;
@@ -28,8 +30,8 @@ public class SnakePatrol : MonoBehaviour
             dir = targetPosition - transform.position;
             dir = dir.normalized;
                     
-            //Si le serpent se rend à sa destination
-            if(canMove && Vector3.Distance(transform.position, targetPosition) > 0.3f)
+            //Si le serpent se rend à sa destination                            Nico : modif 0.3f
+            if(canMove && Vector3.Distance(transform.position, targetPosition) > 0.03f)
             {
                 //Déplacement du serpent
                 transform.Translate(dir * moveSpeed * Time.deltaTime, Space.World);
@@ -38,8 +40,8 @@ public class SnakePatrol : MonoBehaviour
                 animator.SetFloat("VerticalSpeed", dir.y);
             }
 
-            //Si le serpent vient d'arriver à sa destination
-            if(canMove && Vector3.Distance(transform.position, targetPosition) <= 0.3f)
+            //Si le serpent vient d'arriver à sa destination                    Nico : modif 0.3f
+            if(canMove && Vector3.Distance(transform.position, targetPosition) <= 0.03f)
             {
                 canMove = false;
                 StartCoroutine(CalculateNewTarget());
@@ -64,27 +66,40 @@ public class SnakePatrol : MonoBehaviour
         bool isTargetOk = false;
         int obstacleLayer = 1 << 8;
         RaycastHit2D hit;
-
+        int maxTry = 100;
+        
         yield return new WaitForSeconds(movementDelay); //Attente jusqu'à la prochaine possibilité de mouvement
         canMove = true;
         
-        while(!isTargetOk) //Execute tant que la nouvelle position à atteindre n'est pas valide
+        while(!isTargetOk && maxTry > 0) //Execute tant que la nouvelle position à atteindre n'est pas valide
         {
             //Calcul de la nouvelle position à atteindre
             targetPosition = (Random.insideUnitCircle * movementRadius);
             dir = targetPosition;
             targetPosition += transform.position;
 
-            hit = Physics2D.Raycast(transform.position, dir.normalized, Vector2.Distance(transform.position, targetPosition), obstacleLayer);
-
+            //Nico : test Boxcast
+            //hit = Physics2D.Raycast(transform.position, dir.normalized, Vector2.Distance(transform.position, targetPosition), obstacleLayer);
+            hit = Physics2D.BoxCast(transform.position, myCollider.size, 0f, dir.normalized,
+                Vector2.Distance(transform.position, targetPosition), obstacleLayer);
+            
             if (hit) //Vérifie qu'il n'y a pas d'obstacle sur le chemin
             {
                 Debug.DrawRay(transform.position, dir.normalized * hit.distance, Color.red, 2f);
+                maxTry--;
+                if (maxTry == 0) Debug.Log("100 try !");
             }else
             {
                 isTargetOk = true; //Nouvelle position à atteindre valide
                 Debug.DrawRay(transform.position, dir.normalized * Vector2.Distance(transform.position, targetPosition), Color.green, 2f);
             }
         } 
+    }
+
+    //Nico : trace cube
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position + dir.normalized * Vector2.Distance(transform.position, targetPosition), myCollider.size);
     }
 }
